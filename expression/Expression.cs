9159f00c9c27b4945f0419160d99494ba5466136
KillerAll <ragsweb@gmail.com>
2016-11-04 18:38:27 -0200

    Inicio da implementação da tabela de simbulos

---
+++ expression/Expression.cs
@@ -0,0 +1,26 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advpl_parser.expression
+{
+    class Expression
+    {
+        public String Content { get; set; }
+        ParserRuleContext ctx;
+
+        public Expression(ParserRuleContext ctx)
+        {
+            this.ctx = ctx;
+            Content = ctx.GetText();
+        }
+        public Expression()
+        {
+            
+        }
+
+    }
+}
